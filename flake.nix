51ea32a5fd7e1daf5f8276677b33458347fb6d1a
Kerstin Humm <kerstin@erictapen.name>
2022-12-20 17:30:51 +0100

    init flake

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Visualizing Cultural Collections - Restaging Fashion";
+
+  inputs.nixpkgs.url = "github:NixOS/Nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      config = { system = "x86_64-linux"; };
+      pkgs = import nixpkgs config;
+    in
+    {
+
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.webapp;
+
+      packages.x86_64-linux.webapp = let
+        webapp = import ./default.nix {
+            inherit nixpkgs config;
+            revision = self.rev or "dirty";
+          };
+       in pkgs.runCommand "refa" { } ''
+         mkdir -p $out/refa
+         ln -s ${webapp} $out/refa
+       '';
+
+      nixosModule = import ./module.nix self.packages.x86_64-linux.webapp;
+
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = with pkgs; with elmPackages; [
+          elm
+          elm-format
+          elm-test
+          elm-json
+          elm2nix
+          (python3.withPackages (ps: with ps; [
+            pandas
+          ]))
+          (inkscape-with-extensions.override {
+            inkscapeExtensions = with inkscape-extensions; [
+              applytransforms
+            ];
+          })
+        ];
+      };
+
+    };
+}
